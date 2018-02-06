7e943e1c02ab4a36f57ef4a5b654b596538390ab
davidyunius <32894219+davidyunius@users.noreply.github.com>
2018-02-06 15:22:18 +0700

    release 0 dan 1

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,11 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for (let index in values){
+    if(values[index]===target)
+    return index
+  }
+  return -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -15,8 +20,19 @@ console.log(linearSearch(9, random_numbers));
 
 // Release 1
 console.log("Global Linear Search")
-let globalLinearSearch = (target, values) => {
+let globalLinearSearch = (target, values, obj) => {
   //write your code here
+  obj = obj || {i: 0, arr: []}
+  // console.log(values.length)
+  if (obj.i == values.length) {
+    return obj.arr
+  } else {
+    if (values[obj.i] == target) {
+      obj.arr.push(obj.i)
+    }
+    obj.i++
+    return globalLinearSearch(target, values, obj)
+  }
 }
 
 let banana_arr = "banana".split("");
