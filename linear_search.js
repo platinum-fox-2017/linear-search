2083667ab9ffafa9358073741f77f91f71b90504
Andrew Kusuma <andrew.budikusuma@gmail.com>
2018-02-06 13:51:41 +0700

    Release 0 and 1

--- linear_search.js
+++ linear_search.js
@@ -4,24 +4,37 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for (let i = 0; i < values.length; i++) {
+    if (target === values[i]) {
+      return values.indexOf(target)
+    }
+  }
+  return -1
 }
 
-let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
+let random_numbers = [6, 29, 18, 2, 72, 19, 18, 10, 37]
 
-console.log(linearSearch(18, random_numbers));
+console.log(linearSearch(18, random_numbers))
 // 2
-console.log(linearSearch(9, random_numbers));
+console.log(linearSearch(9, random_numbers))
 // -1
 
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  let targetPosition = []
+  for (let i = 0; i < values.length; i++) {
+    if (target === values[i]) {
+      targetPosition.push(i)
+    }
+  }
+  return targetPosition
 }
 
-let banana_arr = "banana".split("");
+let banana_arr = "banana".split("")
 // ["b", "a", "n", "a", "n", "a"]
-console.log(globalLinearSearch("a", banana_arr));
+console.log(globalLinearSearch("a", banana_arr))
 // [ 1, 3, 5 ]
 
 module.exports = {
