0cac05b6835a2cae12db1ce474e3b695e02a265f
Kevin Himawan Hie <kevinhimawanhie@Kevins-MacBook-Pro.local>
2018-02-06 12:54:04 +0700

    updated Linear Search

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,15 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+
+  const result = values.filter(value =>{
+    return value === target
+  })
+  if(result.length ===0){
+    return -1
+  }else{
+    return result.length
+  }
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -14,9 +22,26 @@ console.log(linearSearch(9, random_numbers));
 // -1
 
 // Release 1
-console.log("Global Linear Search")
+// console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  // let tmp = []
+  // for(let [index,value] of values.entries()){
+  //   if(value === target){
+  //     tmp.push(index)
+  //   }
+  // }
+
+  // return tmp
+
+  var iterator = values.keys()
+  let tmp = []
+  for (let key of iterator){
+    if(values[key] === target){
+      tmp.push(key)
+    }
+  }
+
+  return tmp
 }
 
 let banana_arr = "banana".split("");
@@ -24,7 +49,7 @@ let banana_arr = "banana".split("");
 console.log(globalLinearSearch("a", banana_arr));
 // [ 1, 3, 5 ]
 
-module.exports = {
-  linearSearch,
-  globalLinearSearch
-}
+// module.exports = {
+//   linearSearch,
+//   globalLinearSearch
+// }
