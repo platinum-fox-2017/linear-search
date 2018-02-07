9c586f14b823a46ac8ac327f93ddf67187d2136d
ekifakhrureza <ekifakhrureza@gmail.com>
2018-02-07 11:49:07 +0700

    REVISI LINEAR 2

--- linear_search.js
+++ linear_search.js
@@ -1,18 +1,18 @@
 function linearSearch(input) {
   var random_numbers=[6,29,18,2,72,19,18,10,37];
   var hit=0;
-  for(var i=0;i<random_numbers.length;i++)
-  {
-    if(input===random_numbers[i])
-      {
+
+  for (var i=0;i<random_numbers.length;i++){
+    if(input===random_numbers[i]){
         hit++;
-      }
-  }
-  if(hit>0)
-    {
-      return hit;
     }
-    return -1;
+  }
+
+  if (hit>0){
+      return hit;
+  }
+
+  return -1;
 }
 
 console.log(linearSearch(18));
@@ -21,13 +21,13 @@ console.log(linearSearch(9));
 function globalLinearSearch(cari,input) {
   var arr=input.split("");
   var temp=[];
-  for(var i=0;i<=input.length;i++)
-  {
-    if(cari===input[i])
-      {
+
+  for (var i=0;i<=input.length;i++){
+    if (cari===input[i]){
         temp.push(i);
-      }
+    }
   }
+  
   return temp;
 }
 
